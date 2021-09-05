4eff83d5721ab335acec06ff0cce5cedd7671fe9
estebanhirzfeld <estebanhirzfeld@gmail.com>
2021-09-04 21:45:37 -0300

    edicion de js

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,15 @@
+alert("Seleccione Operacion a realizar");
+
+let respuesta = prompt("Elija entre Pesos a Dolares o Dolares a Pesos");
+
+let inputDolares = prompt("Ingresar Monto en Dolares");
+
+let inputPesos = inputDolares * 10;
 
 
-alert("sexo");
+
+alert("Ud. Recibira $" + inputPesos + " Pesos por $" + inputDolares + " Dolares");
+
+console.log("Ud. Recibira $" + inputPesos + " Pesos por $" + inputDolares + " Dolares");
+
+
