32d68089f55ac94900c002aa0cba2463d7ec5cad
estebanhirzfeld <estebanhirzfeld@gmail.com>
2021-09-24 15:33:37 -0300

    Funcionalidad con JavaScript

--- javascript/home.js
+++ javascript/home.js
@@ -3,12 +3,15 @@ function cajaSelectoraPesosToDolar(){
     let inputPesos = document.getElementById("inputPesos").value;
     
     let outputDolares = (inputPesos / 131)-((inputPesos/131)/100)*6;
-    
+
     // console.log("inputPesos: " + inputPesos)
     
     inputDolares = document.getElementById("inputDolares").value = outputDolares.toFixed(2);
 
-    return outputDolares.toFixed(2);
+    if (inputDolares < 0 ) {inputPesos   = document.getElementById("inputPesos").value = 0;  }
+    if (inputDolares < 0 ) {inputDolares = document.getElementById("inputDolares").value = 0;}
+    
+    return inputDolares;
 }
 
 function cajaSelectoraDolarToPesos(){
@@ -19,8 +22,11 @@ function cajaSelectoraDolarToPesos(){
     // console.log("inputDolares: " + inputDolares)
     
     inputPesos = document.getElementById("inputPesos").value = outputPesos.toFixed(2);
-
-    return outputPesos.toFixed(2);
+    
+    if (inputDolares < 0 ) {inputPesos   = document.getElementById("inputPesos").value = 0;  }
+    if (inputDolares < 0 ) {inputDolares = document.getElementById("inputDolares").value = 0;}
+    
+    return inputPesos;
 }
 
 
