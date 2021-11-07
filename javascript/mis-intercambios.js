5976b8758c85e2bdd66778c95058ebfa3411546d
estebanhirzfeld <estebanhirzfeld@gmail.com>
2021-11-07 17:30:23 -0300

    incluido los href de Mis-Intercambios-Page

---
+++ javascript/mis-intercambios.js
@@ -0,0 +1,7 @@
+$(".btnCrearNuevaOfertaMS").click(function(){
+    window.location.replace("../pages/mis-ofertas.html");
+})
+
+$(".btnEncontrarOfertaMS").click(function(){
+    window.location.replace("../pages/explorar.html");
+})
